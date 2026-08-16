be1f9f58895da0b5d2cfb3f752bc2f2ab332e0c5
ym8zsbyhck-oss <ym8zsbyhck@privaterelay.appleid.com>
2026-08-16 10:19:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="rfc06";const A=["./","./index.html","./css/app.css","./js/data.js","./js/logos.js","./js/career.js","./js/match.js","./js/app.js","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
